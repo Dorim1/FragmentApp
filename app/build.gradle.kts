9c9894031266b3853e4469b1d2f50b25e7537145
Dreamy <tolya.lyashhenko.05@mail.ru>
2025-11-09 15:41:24 +0300

    FA-Network-Okhttp-4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.navigation.safeargs)
+    alias(libs.plugins.kotlin.serialization)
     id("kotlin-parcelize")
 }
 
@@ -56,6 +57,7 @@ dependencies {
     implementation(libs.androidx.fragment)
     implementation(libs.google.gson)
     implementation(libs.okhttp)
+    implementation(libs.kotlinx.serialization.json)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
