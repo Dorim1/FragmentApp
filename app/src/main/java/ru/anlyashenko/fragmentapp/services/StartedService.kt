e569a8366b438f38f5613a8f5666274f197f05d2
Dreamy <tolya.lyashhenko.05@mail.ru>
2025-11-18 11:16:37 +0300

    FA-Service-Foreground Service

--- app/src/main/java/ru/anlyashenko/fragmentapp/services/StartedService.kt
+++ app/src/main/java/ru/anlyashenko/fragmentapp/services/StartedService.kt
@@ -1,15 +1,25 @@
 package ru.anlyashenko.fragmentapp.services
 
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.content.pm.ServiceInfo
 import android.content.res.Resources
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import ru.anlyashenko.fragmentapp.R
 import kotlin.concurrent.thread
 
 class StartedService : Service() {
 
     private var serviceThread: Thread? = null
+
+    private val notificationManager by lazy {
+        getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+    }
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
@@ -17,18 +27,75 @@ class StartedService : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.d("Service", "Сервис: onStartCommand. Поток: ${Thread.currentThread().name}")
-        serviceThread = thread {
-            try {
-                Log.d("Service", "Фоновая работа: НАЧАЛАСЬ. Поток: ${Thread.currentThread().name}")
-                Thread.sleep(4000)
-                Log.d("Service", "Фоновая работа: ЗАВЕРШЕНА.")
-                stopSelf()
-            } catch (e: InterruptedException) {
-                Log.d("Service", "Поток был прерван")
+
+        if (intent?.action == ACTION_STOP) {
+            serviceThread?.interrupt()
+            stopForeground(STOP_FOREGROUND_REMOVE)
+            stopSelf()
+            return START_NOT_STICKY
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                startForeground(
+                    NOTIFICATION_ID,
+                    createNotification(0),
+                    ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC
+                )
+            } else {
+                startForeground(NOTIFICATION_ID, createNotification(0))
             }
 
+            serviceThread = thread {
+                try {
+                    for (progress in 0..100 step 10) {
+                        if (Thread.currentThread().isInterrupted) {
+                            break
+                        } else {
+                            Log.d("Service", "Прогресс: $progress")
+                            notificationManager.notify(NOTIFICATION_ID, createNotification(progress))
+                            Thread.sleep(1000)
+                        }
+
+                    }
+                    Log.d("Service", "Фоновая работа: ЗАВЕРШЕНА.")
+                    stopForeground(STOP_FOREGROUND_REMOVE)
+                    stopSelf()
+                } catch (e: InterruptedException) {
+                    Log.d("Service", "Поток был прерван")
+                }
+
+            }
+            return START_NOT_STICKY
         }
-        return START_NOT_STICKY
+    }
+
+    private fun createNotification(progress: Int) : Notification {
+        val intent = Intent(this, StartedService::class.java)
+        intent.action = ACTION_STOP
+        val pendingIntent = PendingIntent.getService(
+            this,
+            0,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT,
+            )
+
+        val builder = Notification.Builder(this, CHANNEL_ID)
+            .setContentTitle("Загрузка данных")
+            .setContentText("Пожалуйста, подождите... $progress%")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setOnlyAlertOnce(true)
+            .addAction(
+                R.drawable.ic_launcher_foreground,
+                "Остановить",
+                pendingIntent
+            )
+
+        if (progress > 0) {
+            builder.setProgress(100, progress, false)
+        } else {
+            builder.setProgress(0, 0, true)
+        }
+
+        return builder.build()
     }
 
     override fun onDestroy() {
@@ -36,3 +103,7 @@ class StartedService : Service() {
         super.onDestroy()
     }
 }
+
+private const val ACTION_STOP = "ru.anlyashenko.stop_service"
+private const val NOTIFICATION_ID = 1
+private const val CHANNEL_ID = "service_channel"
--- app/src/main/java/ru/anlyashenko/fragmentapp/view/ServiceFragment.kt
+++ app/src/main/java/ru/anlyashenko/fragmentapp/view/ServiceFragment.kt
@@ -1,6 +1,11 @@
 package ru.anlyashenko.fragmentapp.view
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -8,15 +13,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
 import ru.anlyashenko.fragmentapp.R
 import ru.anlyashenko.fragmentapp.databinding.FragmentServiceBinding
 import ru.anlyashenko.fragmentapp.services.StartedService
 
 class ServiceFragment : Fragment() {
 
-    private var _binding : FragmentServiceBinding? = null
+    private var _binding: FragmentServiceBinding? = null
     private val binding
         get() = _binding!!
+
+    private val requestPermissionsLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            startService()
+        } else {
+            Toast.makeText(requireContext(), "Нужно разрешение на уведомления!", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,10 +47,18 @@ class ServiceFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        createNotificationChannel()
+
         binding.btnStartService.setOnClickListener {
-            Log.d("ServiceFragment", "send the startService command")
-            val intent = Intent(requireContext(), StartedService::class.java)
-            requireActivity().startService(intent)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+                    startService()
+                } else {
+                    requestPermissionsLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                }
+            } else {
+                startService()
+            }
         }
 
         binding.btnStopService.setOnClickListener {
@@ -45,6 +71,24 @@ class ServiceFragment : Fragment() {
             Toast.makeText(requireContext(), "UI отвечает!", Toast.LENGTH_SHORT).show()
         }
 
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "service_channel"
+            val channelName = "Канал сервиса" // Исправил опечатку "сервера" -> "сервиса"
+            val importance = NotificationManager.IMPORTANCE_LOW
+            val channel = NotificationChannel(channelId, channelName, importance)
+            channel.description = "Системный канал для фонового сервиса"
+            val notificationManager = ContextCompat.getSystemService(requireContext(), NotificationManager::class.java)
+            notificationManager?.createNotificationChannel(channel)
+        }
+    }
+
+    private fun startService() {
+        Log.d("ServiceFragment", "send the startService command")
+        val intent = Intent(requireContext(), StartedService::class.java)
+        ContextCompat.startForegroundService(requireContext(), intent)
     }
 
     override fun onDestroyView() {
